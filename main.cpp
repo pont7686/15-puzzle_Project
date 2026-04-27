bf283801295d5ca86a39c6d99dc3fb7c7ebd316b
Ryan <149628271+pont7686@users.noreply.github.com>
2026-04-27 20:50:08 +0000

    c2

---
+++ main.cpp
@@ -0,0 +1,166 @@
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+#include <queue>
+#include <set>
+#include <string>
+
+using namespace std;
+
+const int N = 4;
+
+struct State {
+    int board[N][N];
+    int g, h, f;
+    string path;
+
+    bool operator > (const State& other) const {
+        return f > other.f;
+    }
+};
+
+int goal[N][N] = {
+    {1, 2, 3, 4},
+    {5, 6, 7, 8},
+    {9,10,11,12},
+    {13,14,15,0}
+};
+
+string boardToString(int board[N][N]) {
+    string s = "";
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            s += to_string(board[i][j]) + ",";
+    return s;
+}
+
+void copyBoard(int boardA[N][N], int boardB[N][N]) {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            boardB[i][j] = boardA[i][j];
+}
+
+void printBoard(int board[N][N]) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            cout << board[i][j];
+            if (board[i][j] < 10) {
+                cout << "  ";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+bool isGoal(int board[N][N]) {
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (board[i][j] != goal[i][j])
+                return false;
+    return true;
+}
+
+bool isSolvable(int board[N][N]) {
+    int array[16];
+    int k = 0;
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            array[k++] = board[i][j];
+
+    int inversions = 0;
+    for (int i = 0; i < 15; i++) {
+        for (int j = i + 1; j < 16; j++) {
+            if (array[i] && array[j] && array[i] > array[j])
+                inversions++;
+        }
+    }
+
+    int blankRow;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            if (board[i][j] == 0)
+                blankRow = i;
+
+    int row = N - blankRow;
+
+    if (row % 2 == 0)
+        return inversions % 2 == 1;
+    else
+        return inversions % 2 == 0;
+}
+
+void inputBoard(int board[N][N]) {
+    cout << "Enter your board:" << endl;
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++)
+            cin >> board[i][j];
+}
+
+void randomize(int board[N][N]) {
+    copyBoard(goal, board);
+    
+    int directionX[4] = {-1,1,0,0};
+    int directionY[4] = {0,0,-1,1};
+    
+    int x = 3, y = 3;
+    
+    for (int i = 0; i < 100; i++) {
+        int direction = rand() % 4;
+        
+        int nextX = x + directionX[direction];
+        int nextY = y + directionY[direction];
+        
+        if (nextX >= 0 && nextX < N && nextY >= 0 && nextY < N) {
+            int temp = board[x][y];
+            board[x][y] = board[nextX][nextY];
+            board[nextX][nextY] = temp;
+            
+            x = nextX;
+            y = nextY;
+        }
+    }
+}
+
+int manhattan(int board[N][N]) {
+    int dist = 0;
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            int val = board[i][j];
+            if (val != 0) {
+                int X = (val - 1) / N;
+                int Y = (val - 1) % N;
+                dist += abs(i - X) + abs(j - Y);
+            }
+        }
+    }
+    return dist;
+}
+
+int main() {
+    int board[N][N];
+    int choice, algorithm;
+    
+    cout << "15-Puzzle Problem:" << endl;
+    cout << "1. Create a puzzle" << endl;
+    cout << "2. Random a puzzle" << endl;
+    cout << "3. Exit" << endl;
+    cin >> choice;
+    
+    switch (choice) {
+        case 1:
+            inputBoard(board);
+            cout << "Created board:" << endl;
+            printBoard(board);
+            break;
+        case 2:
+            randomize(board);
+
+    }
+
+    return 0;
+}
